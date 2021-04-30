54b5ccdca9b859b8027c05ef51eaaa26e2982b9c
zhaojie <1979170837@qq.com>
2021-04-30 13:50:32 +0800

    update

--- java/javax/servlet/Servlet.java
+++ java/javax/servlet/Servlet.java
@@ -53,6 +53,13 @@ import java.io.IOException;
  *
  * @see GenericServlet
  * @see javax.servlet.http.HttpServlet
+ *
+ * @apiNote 是用Java编写的服务器端程序。其主要功能在于交互式地浏览和修改数据，生成动态Web内容。
+ * 狭义的Servlet是指Java语言实现的一个接口，广义的Servlet是指任何实现了这个Servlet接口的类，一般情况下，人们将Servlet理解为后者。
+ *
+ * <p> Servlet运行于支持Java的应用服务器中。从实现上讲，Servlet可以响应任何类型的请求，但绝大多数情况下Servlet只用来扩展基于HTTP协议的Web服务器。
+ *
+ * <p> Servlet接口定义了Servlet与servlet容器之间的契约。这个契约是：Servlet容器将Servlet类载入内存，并产生Servlet实例和调用它具体的方法。但是要注意的是，在一个应用程序中，每种Servlet类型只能有一个实例。
  */
 public interface Servlet {
 
@@ -84,6 +91,10 @@ public interface Servlet {
      *
      * @see UnavailableException
      * @see #getServletConfig
+     *
+     * @apiNote Servlet的生命周期方法，执行Servlet的一些初始化工作
+     * 当Servlet第一次被请求时，Servlet容器就会开始调用这个方法来初始化一个Servlet对象出来，但是这个方法在后续请求中不会在被Servlet容器调用，就像人只能“出生”一次一样。
+     * 我们可以利用init()方法来执行相应的初始化工作。调用这个方法时，Servlet容器会传入一个ServletConfig对象进来从而对Servlet对象进行初始化。
      */
     public void init(ServletConfig config) throws ServletException;
 
@@ -144,6 +155,11 @@ public interface Servlet {
      *
      * @exception IOException
      *                if an input or output exception occurs
+     *
+     * @apiNote Servlet的生命周期方法，执行Servlet的业务逻辑
+     * 每当请求Servlet时，Servlet容器就会调用这个方法。就像人一样，需要不停的接受老板的指令并且“工作”。
+     * 第一次请求时，Servlet容器会先调用init()方法初始化一个Servlet对象出来，然后会调用它的service()方法进行工作，
+     * 但在后续的请求中，Servlet容器只会调用service方法了。
      */
     public void service(ServletRequest req, ServletResponse res)
             throws ServletException, IOException;
@@ -173,6 +189,10 @@ public interface Servlet {
      * that are being held (for example, memory, file handles, threads) and make
      * sure that any persistent state is synchronized with the servlet's current
      * state in memory.
+     *
+     * @apiNote Servlet的生命周期方法，销毁Servlet
+     * 当要销毁Servlet时，Servlet容器就会调用这个方法，就如人一样，到时期了就得死亡。
+     * 在卸载应用程序或者关闭Servlet容器时，就会发生这种情况，一般在这个方法中会写一些清除代码。
      */
     public void destroy();
 }
