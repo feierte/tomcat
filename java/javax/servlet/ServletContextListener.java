61c8705e97ae5f82afb970c047c7cf182de2f829
HW <1979170837@qq.com>
2024-09-30 14:49:45 +0800

    tomcat servlet

--- java/javax/servlet/ServletContextListener.java
+++ java/javax/servlet/ServletContextListener.java
@@ -27,7 +27,13 @@ import java.util.EventListener;
  * @see ServletContextEvent
  * @since v 2.3
  *
- * @apiNote Servlet全局监听器
+ * @apiNote ServletContext 监听器，用于监听 ServletContext 对象的生命周期。当 Servlet 容器启动或终止 Web 应用程序时，会触发 ServletContextEvent 事件。
+ *
+ * <p> 应用场景 </p>
+ * 全局数据初始化：在服务器启动时，可以初始化一些全局数据，如数据库连接池，并将其存储在 ServletContext 中供其他组件使用。
+ * 资源清理：在服务器关闭前，进行资源的清理工作，如关闭数据库连接。
+ * 框架初始化：许多框架（如 Spring）使用 ServletContextListener 来加载配置文件并初始化框架。
+ * 定时任务调度：可以启动定时任务，例如每天晚上执行数据同步或清理任务。
  */
 public interface ServletContextListener extends EventListener {
 
