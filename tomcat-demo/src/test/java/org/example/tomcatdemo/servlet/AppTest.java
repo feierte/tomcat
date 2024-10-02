57588d50e2146ae1e0fb19e9b6ff668bd57d6b48
HW <1979170837@qq.com>
2024-10-02 10:18:12 +0800

    tomcat demo

---
+++ tomcat-demo/src/test/java/org/example/tomcatdemo/servlet/AppTest.java
@@ -0,0 +1,15 @@
+package org.example.tomcatdemo.servlet;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Jie Zhao
+ * @date 2024/10/2 10:00
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        System.out.println("test!");
+    }
+}
